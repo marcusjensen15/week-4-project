5d695fd727e1df7598eddc2e35a02d4dce55de9a
Marcus Jensen <marcusjensen1515@gmail.com>
2019-11-08 09:40:30 -0800

    Functions now pulling correct values from checkboxes and calculating total toppings price

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+var checkedToppings = [];
+
+
 Pizza = function(name, toppings, size){
   this.name = name,
   this.size = size,
@@ -21,13 +24,24 @@ PizzaManager.prototype.addPizza = function (pizzaInput){
 var myPizza = new Pizza("Marcus",["tomato","olive","pep"], "large");
 var pizzaManager = new PizzaManager();
 
-pizzaManager.addPizza(myPizza);
+// pizzaManager.addPizza(myPizza);
+//
+// console.log(pizzaManager);
 
-console.log(pizzaManager);
+function toppingsValue(toppings){
+  for(i=0; i < toppings.length; i++){
+    checkedToppings.push(parseInt(toppings[i].value));
+  }
+};
 
+function sumToppings(checkedToppingsArray){
+  var total = 0;
+  for (i=0 ; i < checkedToppingsArray.length; i++){
+    total += checkedToppingsArray[i];
+  }
+    return total;
 
-
-
+}
 
 
 $( document ).ready(function() {
@@ -35,6 +49,11 @@ $( document ).ready(function() {
 
       var pizzaSize = parseInt($("input[name='size']:checked").val());
       console.log(pizzaSize);
+      var toppings = $('input[type=checkbox]:checked');
+      toppingsValue(toppings);
+      console.log(checkedToppings);
+      console.log(sumToppings(checkedToppings));
+
 
 
         event.preventDefault();
