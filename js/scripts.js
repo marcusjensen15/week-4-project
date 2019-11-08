3413b10bf367f8db59a24d9639e587369eb40d68
Marcus Jensen <marcusjensen1515@gmail.com>
2019-11-08 09:05:28 -0800

    Now able to add pizzas to the pizzaManager object in the js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,35 @@
+Pizza = function(name, toppings, size){
+  this.name = name,
+  this.size = size,
+  this.toppings = toppings,
+  this.price = 0
+
+};
+
+PizzaManager = function (){
+
+  this.pizzas = [];
+
+};
+
+PizzaManager.prototype.addPizza = function (pizzaInput){
+
+  this.pizzas.push(pizzaInput);
+
+};
+
+var myPizza = new Pizza("Marcus",["tomato","olive","pep"], "large");
+var pizzaManager = new PizzaManager();
+
+pizzaManager.addPizza("hello");
+
+console.log(pizzaManager);
+
+
+
+
+
+
 $( document ).ready(function() {
     $("#pizzaOrder").submit(function(event){
 
